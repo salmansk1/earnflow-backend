b1b1fb135bb38a5f996109fab804f7f838b13455
salmansk1 <120190696+salmansk1@users.noreply.github.com>
2026-04-24 21:06:01 +0500

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -1,10 +1,8 @@
 export default function handler(req, res) {
-  console.log("FULL QUERY:", req.query);
-
   const user_id =
     req.query.user_id ||
     req.query.userId ||
-    req.query.uid;
+    req.query.aff_sub;
 
   const amount =
     req.query.amount ||
@@ -14,11 +12,14 @@ export default function handler(req, res) {
   const txid =
     req.query.txid ||
     req.query.transaction_id ||
-    req.query.txId;
+    req.query.click_id;
 
   if (!user_id || !amount || !txid) {
     return res.send("0");
   }
 
-  return res.send("1");
+  // redirect to dashboard with coins
+  res.redirect(`https://earnflow123.rf.gd/dashboard.html?coins=${amount}`);
 }
+
+
